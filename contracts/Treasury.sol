c963927ff3b5a72be98d7e98e2d5a3b287fdcd36
IntegralTeam O <repo@sfxdx.ru>
2021-03-15 13:05:22 +0300

    adding buildable treasury contract

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -13,14 +13,13 @@ import "./interfaces/ITreasury.sol";
 import "./interfaces/IOneSplitAudit.sol";
 
 /// @title Treasury
-/// @notice
+/// @notice Realisation of ITreasury for channeling managing fees from strategies to gov and governance address
 contract Treasury is Governable, Initializable, Context, ITreasury {
 
-    using SafeMath for uint256256;
+    using SafeMath for uint256;
     using Address for address;
     using SafeERC20 for IERC20;
 
-    address public governance;
     address public oneSplit;
     address public governanceContract;
     address public rewardsToken;
@@ -32,9 +31,17 @@ contract Treasury is Governable, Initializable, Context, ITreasury {
       _;
     }
 
-    function configure(address _governance, address _oneSplit) external initializer {
+    function configure(
+        address _governance,
+        address _oneSplit,
+        address _governanceContract,
+        address _rewardsToken
+      ) external initializer {
         setGovernance(_governance);
         setOneSplit(_oneSplit);
+        setGovernanceContract(_governanceContract);
+        setRewardsToken(_rewardsToken);
+        setAuthorized(_governance);
     }
 
     function setOneSplit(address _oneSplit) public onlyGovernance {
@@ -49,19 +56,19 @@ contract Treasury is Governable, Initializable, Context, ITreasury {
         authorized[_authorized] = true;
     }
 
-    function setGovernanceContract(address _governanceContract) public onlyGovernance {
-      governanceContract = _governanceContract;
-    }
-
     function revokeAuthorized(address _authorized) external onlyGovernance {
         authorized[_authorized] = false;
     }
 
-    function toGovernance(address _token, uint256 _amount) external onlyGovernance {
+    function setGovernanceContract(address _governanceContract) public onlyGovernance {
+        governanceContract = _governanceContract;
+    }
+
+    function toGovernance(address _token, uint256 _amount) override external onlyGovernance {
         IERC20(_token).safeTransfer(governance, _amount);
     }
 
-    function toVoters() external {
+    function toVoters() override external {
         uint256 _balance = IERC20(rewardsToken).balanceOf(address(this));
         IERC20(rewardsToken).safeApprove(governanceContract, 0);
         IERC20(rewardsToken).safeApprove(governanceContract, _balance);
@@ -70,17 +77,17 @@ contract Treasury is Governable, Initializable, Context, ITreasury {
 
     function getExpectedReturn(address _from, address _to, uint256 parts) external view returns(uint256 expected) {
         uint256 _balance = IERC20(_from).balanceOf(address(this));
-        (expected,) = OneSplitAudit(onesplit).getExpectedReturn(_from, _to, _balance, parts, 0);
+        (expected,) = IOneSplitAudit(oneSplit).getExpectedReturn(_from, _to, _balance, parts, 0);
     }
 
     // Only allows to withdraw non-core strategy tokens ~ this is over and above normal yield
-    function convert(address _from, uint256 _parts) external authorizedOnly {
+    function convert(address _from, uint256 _parts) override external authorizedOnly {
         uint256 _amount = IERC20(_from).balanceOf(address(this));
         uint256[] memory _distribution;
         uint256 _expected;
         IERC20(_from).safeApprove(oneSplit, 0);
         IERC20(_from).safeApprove(oneSplit, _amount);
         (_expected, _distribution) = IOneSplitAudit(oneSplit).getExpectedReturn(_from, rewardsToken, _amount, _parts, 0);
-        IOneSplitAudit(onesplit).swap(_from, rewards, _amount, _expected, _distribution, 0);
+        IOneSplitAudit(oneSplit).swap(_from, rewardsToken, _amount, _expected, _distribution, 0);
     }
 }

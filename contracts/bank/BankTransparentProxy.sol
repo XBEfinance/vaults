88cccc52a5e6d8036726ccac25baa5c5836ce8bb
IntegralTeam O <repo@sfxdx.ru>
2021-04-02 17:27:10 +0300

    remove some comments

--- contracts/bank/BankTransparentProxy.sol
+++ contracts/bank/BankTransparentProxy.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol"
 
 
-/// @title Bank
-/// @notice EURxb Token Exchange Contract
 contract BankTransparentProxy is TransparentUpgradeableProxy {
 
     constructor(address _bank, address _bankAdminContract)

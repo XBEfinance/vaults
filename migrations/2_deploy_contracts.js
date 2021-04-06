6065b146e5798b76c93951c76269a871ffe9fdae
IntegralTeam O <repo@sfxdx.ru>
2021-04-06 18:47:29 +0300

    fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,3 @@
-const { BN } = require('@openzeppelin/test-helpers');
-
-const ether = (n) => new BN(web3.utils.toWei(n, 'ether'));
-const ONE = new BN('1');
-const ZERO = new BN('0');
-
 const Treasury = artifacts.require('Treasury');
 const Governance = artifacts.require('Governance');
 const XBE = artifacts.require('XBE');
@@ -44,32 +38,19 @@ module.exports = function (deployer, network, accounts) {
     } else if (network === "rinkeby_safe_test") {
       const TestExecutor = artifacts.require("TestExecutor");
       const MockContract = artifacts.require("MockContract");
-      const Governance = artifacts.require("Governance");
       const MockToken = artifacts.require("MockToken");
       const IGnosisSafe = artifacts.require("IGnosisSafe");
-
-      const safeAddress = "0xD78D94634d8F2E3eFADE97A5D13Da04c92440e67";
 
       const firstOwner = accounts[0];
       const alice = accounts[3];
 
       const aliceAmount = ether('0.01');
 
-      var mock;
-      var mockToken;
-      var governanceContract;
-      var safe;
-      var executor;
-
-      var proposalId;
-
       const proposalHash = "some proposal hash";
-      const minumumForVoting = ether('0.1');
+      const minumumForVoting = ether('0.01');
       const quorumForVoting = ONE;
       const periodForVoting = new BN('2');
       const lockForVoting = new BN('2');
-
-      const safe = await IGnosisSafe.at(safeAddress);
 
       const safe = await IGnosisSafe.at("0xD78D94634d8F2E3eFADE97A5D13Da04c92440e67");
       const mock = await deployer.deploy(MockContract);
@@ -98,9 +79,13 @@ module.exports = function (deployer, network, accounts) {
       await governanceContract.setQuorum(quorumForVoting);
       await governanceContract.setLock(lockForVoting);
 
+      const minimumForProposal = minumumForVoting.mul(new BN('2'));
+      await mockToken.approve(governanceContract.address, minimumForProposal);
+      await governanceContract.stake(minimumForProposal);
+
       await governanceContract.register();
 
-      proposalId = await governanceContract.proposalCount();
+      const proposalId = await governanceContract.proposalCount();
       await governanceContract.propose(executor.address, proposalHash);
       await governanceContract.voteFor(proposalId);
 
@@ -124,23 +109,25 @@ module.exports = function (deployer, network, accounts) {
 
 
     } else if (network.startsWith('rinkeby')) {
-      const xbe = await XBE.at('0xfaC2D38F064A35b5C0636a7eDB4B6Cc13bD8D278');
-      const governance = await deployer.deploy(Governance);
-      await governance.configure(
-        '0',
-        xbe.address, // Reward token
-        process.env.RINKEBY_OWNER_ACCOUNT,
-        xbe.address, // Governance token
-        process.env.REWARD_DISTRIBUTION_RINKEBY_ACCOUNT,
-      );
+      if (process.env.RINKEBY_OWNER_ACCOUNT && process.env.REWARD_DISTRIBUTION_RINKEBY_ACCOUNT) {
+        const xbe = await XBE.at('0xfaC2D38F064A35b5C0636a7eDB4B6Cc13bD8D278');
+        const governance = await deployer.deploy(Governance);
+        await governance.configure(
+          '0',
+          xbe.address, // Reward token
+          process.env.RINKEBY_OWNER_ACCOUNT,
+          xbe.address, // Governance token
+          process.env.REWARD_DISTRIBUTION_RINKEBY_ACCOUNT,
+        );
 
-      const treasury = await deployer.deploy(Treasury);
-      await treasury.configure(
-        process.env.RINKEBY_OWNER_ACCOUNT,
-        '0x0000000000000000000000000000000000000000', // testnet OneSplit account
-        governance.address,
-        xbe.address, // Reward token
-      );
+        const treasury = await deployer.deploy(Treasury);
+        await treasury.configure(
+          process.env.RINKEBY_OWNER_ACCOUNT,
+          '0x0000000000000000000000000000000000000000', // testnet OneSplit account
+          governance.address,
+          xbe.address, // Reward token
+        );
+      }
     }
   });
 }

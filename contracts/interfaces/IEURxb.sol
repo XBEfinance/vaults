969763e769d13a17cf80644e88d765be08e20434
IntegralTeam V <repo@sfxdx.ru>
2021-04-26 21:30:23 +0700

    refactoring Bank contract

--- contracts/interfaces/IEURxb.sol
+++ contracts/interfaces/IEURxb.sol
@@ -1,7 +1,8 @@
 pragma solidity >=0.6.0 <0.7.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IEURxb {
+interface IEURxb is IERC20 {
     function mint(address account, uint256 value) external;
     function burn(address account, uint256 value) external;
     function addNewMaturity(uint256 amount, uint256 maturityEnd) external;

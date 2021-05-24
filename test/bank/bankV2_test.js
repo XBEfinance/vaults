2012a5c5fb230be009f7034b5bf0d4c6b13a42f8
IntegralTeam <repo@sfxdx.ru>
2021-05-24 12:24:36 +0300

    BankV2 test fixes

--- test/bank/bankV2_test.js
+++ test/bank/bankV2_test.js
@@ -3,6 +3,7 @@ const {
   expectEvent,
   expectRevert,
   ether,
+  BN,
   time,
 } = require('@openzeppelin/test-helpers');
 
@@ -11,6 +12,7 @@ const EURxb = artifacts.require('EURxb');
 const DDP = artifacts.require('DDP');
 const BondToken = artifacts.require('BondToken');
 const SecurityAssetToken = artifacts.require('SecurityAssetToken');
+const AllowList = artifacts.require('AllowList');
 
 contract('BankV2', (accounts) => {
   const owner = accounts[0];
@@ -19,31 +21,44 @@ contract('BankV2', (accounts) => {
   const alice = accounts[3];
 
   beforeEach(async () => {
+    this.ETHER_100 = web3.utils.toWei('100', 'ether');
+    this.ETHER_0 = web3.utils.toWei('0', 'ether');
+    this.DATE_SHIFT = new BN('10000');
+    this.TOKEN_1 = new BN('1');
+    this.TOKEN_2 = new BN('2');
+
+    this.list = await AllowList.new(owner);
     this.ddp = await DDP.new(owner, { from: owner });
     this.eurxb = await EURxb.new(owner, { from: owner });
     this.bond = await BondToken.new('http://google.com', { from: owner });
-    this.sat = await SecurityAssetToken.new('http://google.com', owner, this.bond.address, owner, { from: owner });
+    this.sat = await SecurityAssetToken.new('http://google.com', owner, this.bond.address, this.list.address, { from: owner });
     this.bankV2 = await BankV2.new({ from: owner });
 
-    await this.ddp.configure(this.bond.address, this.eurxb.address, owner);
-    await this.bond.configure(owner, this.sat.address, this.ddp.address);
+    await this.ddp.configure(this.bond.address, this.eurxb.address, this.list.address);
+    await this.bond.configure(this.list.address, this.sat.address, this.ddp.address);
     await this.eurxb.configure(this.ddp.address, { from: owner });
     await this.bankV2.configure(this.eurxb.address, this.ddp.address, '0x0000000000000000000000000000000000000002', { from: owner });
 
-    await this.sat.mint(client, ether('2'), { from: owner });
-    // console.log(this.eurxb.address);
-    // await this.eurxb.addNewMaturity(ether('1000000'), await time.latest());
-
-    // console.log(this.bankV2.address);
+    await this.list.allowAccount(client);
   });
 
   describe('deposit', () => {
-    it('should fail if amount is less than or equal to zero', async () => {
+    it('should fail', async () => {
+      assert(
+        !(await this.bond.hasToken(this.TOKEN_1)),
+        'bond token must not exist at this time point',
+      );
+      await this.sat.mint(client, this.ETHER_100, this.DATE_SHIFT, { from: owner });
       // expectRevert(await this.bankV2.deposit(this.eurxb.address, '0', '1740310404', { from: owner }));
     });
 
     it('should be ok', async () => {
-      await this.eurxb.transfer(client, ether('1.5'), { from: minter });
+      assert(
+        !(await this.bond.hasToken(this.TOKEN_1)),
+        'bond token must not exist at this time point',
+      );
+      await this.sat.mint(client, this.ETHER_100, this.DATE_SHIFT, { from: owner });
+      // await this.eurxb.transfer(client, ether('1.5'), { from: minter });
       await this.eurxb.approve(this.bankV2.address, ether('1.5'), { from: client });
       await this.bankV2.deposit(this.eurxb.address, ether('1'), '1740310404', { from: client });
     });

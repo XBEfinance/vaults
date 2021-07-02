c6b028fa7e79e008fff9e6a598f34de7404254d7
IntegralTeam E <repo@sfxdx.ru>
2021-07-02 11:02:53 +0300

    fix rinkeby-rpc.sh

--- scripts/rinkeby-rpc.sh
+++ scripts/rinkeby-rpc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ ! -f .env ]
-then
-  export $(cat .env | xargs)
-fi
+
+set -a # automatically export all variables
+source .env
+set +a
+
 ganache-cli --port 8545 --gasLimit 100000000 --accounts 10 --hardfork istanbul --mnemonic "pattern lumber mystery retreat answer wheel seminar adult practice air bring essence" --fork https://rinkeby.infura.io/v3/$INFURA_ID --unlock 0xf0774219823852e445fb592f15d13a336315a0f8 --chainId 1

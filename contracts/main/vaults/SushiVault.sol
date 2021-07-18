77969be13e56f798cfd2142f064b600b7a9265b9
IntegralTeam E <repo@sfxdx.ru>
2021-07-19 01:53:21 +0300

    Public configuration method for sushiVault

--- contracts/main/vaults/SushiVault.sol
+++ contracts/main/vaults/SushiVault.sol
@@ -6,4 +6,24 @@ import "./base/BaseVault.sol";
 /// @notice Vault for staking LP Sushiswap and receive rewards in CVX
 contract SushiVault is BaseVault {
     constructor() BaseVault("XBE Sushi LP", "xs") public {}
+
+    function configure(
+        address _initialToken,
+        address _initialController,
+        address _governance,
+        uint256 _rewardsDuration,
+        address[] memory _rewardTokens,
+        string memory __namePostfix,
+        string memory __symbolPostfix
+    ) public initializer virtual {
+        _configure(
+            _initialToken,
+            _initialController,
+            _governance,
+            _rewardsDuration,
+            _rewardTokens,
+            __namePostfix,
+            __symbolPostfix
+        );
+    }
 }

c9ba51a48a84da096f832178c2252fe76bad4b42
IntegralTeam Y <repo@sfxdx.ru>
2021-07-22 23:23:16 +0300

    add weth9 contract, compile it
    Signed-off-by: IntegralTeam Y <repo@sfxdx.ru>

--- scripts/main_parts_build.sh
+++ scripts/main_parts_build.sh
@@ -9,6 +9,9 @@ source ./scripts/utils/generate_truffle_config.sh
 generate_truffle_config "0.4.24" ".\/contracts\/governance"
 truffle compile
 
+generate_truffle_config "0.4.24" ".\/contracts\/main\/weth"
+truffle compile
+
 # node --stack-size=1200 ./node_modules/.bin/truffle compile
 
 generate_truffle_config "0.6.6" ".\/contracts\/main"

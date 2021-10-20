74cb6636e98ee50c70f677c5fd024e8fe57168d0
IntegralTeam O <repo@sfxdx.ru>
2021-10-20 12:59:11 +0300

    fixed cvx crv strategy

--- contracts/main/strategies/CvxCrvStrategy.sol
+++ contracts/main/strategies/CvxCrvStrategy.sol
@@ -7,16 +7,24 @@ import "./base/ClaimableStrategy.sol";
 import "../interfaces/IBooster.sol";
 import "../interfaces/IRewards.sol";
 import "../interfaces/ICVXRewards.sol";
+import "../interfaces/curve/ICurveCvxCrvStableSwap.sol";
 import "../interfaces/vault/IVaultTransfers.sol";
 
 /// @title CvxCrvStrategy
 contract CvxCrvStrategy is ClaimableStrategy {
+
+    uint256 public constant MAX_BPS = 10000;
+
     struct Settings {
         address crvRewards;
         address cvxRewards;
         address convexBooster;
         address crvDepositor;
         address crvToken;
+        address cvxCrvToken;
+        address curveCvxCrvStableSwapPool;
+        uint256 curveCvxCrvIndexInStableSwapPool;
+        uint256 curveAddLiquiditySlippageTolerance; // in bps, ex: 9500 == 5%
         uint256 poolIndex;
     }
 
@@ -115,10 +123,30 @@ contract CvxCrvStrategy is ClaimableStrategy {
 
     function convertAndStakeTokens(uint256 _amount) external {
         _convertTokens(_amount);
+
+        uint256 cvxCrvBalance = IERC20(poolSettings.cvxCrvToken).balanceOf(address(this));
+        uint256[2] memory _amounts;
+        _amounts[poolSettings.curveCvxCrvIndexInStableSwapPool] = cvxCrvBalance;
+
+        ICurveCvxCrvStableSwap stableSwapPool = ICurveCvxCrvStableSwap(
+          poolSettings.curveCvxCrvStableSwapPool
+        );
+
+        uint256 curveCvxCrvLPAmount = stableSwapPool.calc_token_amount(_amounts, true);
+
+        uint256 minCurveCvxCrvLPAmount = curveCvxCrvLPAmount
+          .mul(poolSettings.curveAddLiquiditySlippageTolerance)
+          .div(MAX_BPS);
+
+        uint256 actualCurveCvxCrvLPAmount = stableSwapPool.add_liquidity(
+            _amounts,
+            minCurveCvxCrvLPAmount,
+            address(this)
+        );
+
         IERC20 _stakingToken = IERC20(_want);
         address vault = IController(controller).vaults(_want);
-        uint256 cvxCrvAmount = _stakingToken.balanceOf(address(this));
-        _stakingToken.approve(vault, cvxCrvAmount);
-        IVaultTransfers(vault).depositFor(cvxCrvAmount, msg.sender);
+        _stakingToken.approve(vault, actualCurveCvxCrvLPAmount);
+        IVaultTransfers(vault).depositFor(actualCurveCvxCrvLPAmount, msg.sender);
     }
 }

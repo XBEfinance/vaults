3c66a2fdb799c73f04f859f0f55b248e0d6b19b4
bonesc-coder <58365253+bonesc-coder@users.noreply.github.com>
2022-03-19 19:41:21 +0200

    remove of file

--- scripts/mainnet_rpc_alchemy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -a # automatically export all variables
-source .env
-set +a
-
-ganache-cli --accounts 100 -e 1000 --fork $ALCHEMY_URL --mnemonic "enforce creek agree hood iron excuse pride movie fluid water oven shield"
